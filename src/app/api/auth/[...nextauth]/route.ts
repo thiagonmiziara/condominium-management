373c3b260601c28bafab473a74421562a283f0e6
thiagonmiziara <thiagonmiziara@gmail.com>
2025-04-12 14:47:49 -0300

    fix: removendo export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import { UserRole } from "@prisma/client";
 import nodemailer from "nodemailer";
 import { prisma } from "@/lib/prisma";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
 
   // Configura o provedor de Email
